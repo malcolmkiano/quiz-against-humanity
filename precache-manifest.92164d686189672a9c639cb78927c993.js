a4b60eff28134bbad6a8fea98dcee1e1ad27f8b0
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-21 00:50:51 -0400

    Updates

---
+++ precache-manifest.92164d686189672a9c639cb78927c993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d084b1221cd06cf8446ee28b649e27",
+    "url": "/quiz-against-humanity/index.html"
+  },
+  {
+    "revision": "062e113a862e05f0b575",
+    "url": "/quiz-against-humanity/static/css/main.b3cd4639.chunk.css"
+  },
+  {
+    "revision": "8daba0cba1b003e5c9ec",
+    "url": "/quiz-against-humanity/static/js/2.c2a7428c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/quiz-against-humanity/static/js/2.c2a7428c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062e113a862e05f0b575",
+    "url": "/quiz-against-humanity/static/js/main.eca149c9.chunk.js"
+  },
+  {
+    "revision": "e7259eb60922acf35f45",
+    "url": "/quiz-against-humanity/static/js/runtime-main.e5a1e15d.js"
+  }
+]);
